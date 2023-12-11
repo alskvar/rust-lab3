323c9ab04fbcd9bf6fa994958cc314c9791d97ed
Aliaksandr Skvarniuk <aliaksandr.skvarniuk@student.uj.edu.pl>
2023-12-11 13:45:08 +0100

    Done

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,35 @@
+use std::ffi::CString;
 use clap::Parser;
 use std::path::PathBuf;
 
-// use ju_tcs_rust_23_25::{head, tail};
+use ju_tcs_rust_23_25::{head, tail};
+// use lib::ju-tcs-rust-23-25::{head, tail};
+
+
+
+// extern "ju-tcs-rust-23-25"
 #[derive(clap::Parser)]
 enum Cmd{
-    Head{n: u32, file: PathBuf},
-    Tail{n: u32, file: PathBuf}
+    Head{n: usize, file: PathBuf},
+    Tail{n: usize, file: PathBuf}
 }
 
-fn head(_:u32, _:PathBuf){todo!();}
-fn tail(_: u32, _: PathBuf){todo!();}
+
+// fn head(_:u32, _:PathBuf){todo!();}
+// fn tail(_: u32, _: PathBuf){todo!();}
 
 fn main() {
     let cmd = Cmd::parse();
+    let mut res: Vec<String>;
     match cmd{
         Cmd::Head{n, file} => {
-            head(n, file);
+            res = head(file.as_path(), n);
         }
         Cmd::Tail{n, file} => {
-            tail(n, file);
+           res = tail(file.as_path(), n);
         }
+    }
+    for i in res{
+        println!("{}", i);
     }
 }
